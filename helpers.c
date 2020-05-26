546eee19450bfc661f3959600564ff3b548fe28c
SimonaKolarova <SimonaKolarova@users.noreply.github.com>
2020-05-26 13:28:16 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -71,7 +71,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     // Loop over each pixel on the left-hand side of the image
         for (int i = 0; i < height; i++)
         {
-            for (int k = 0; k <= width/2; k++)
+            for (int k = 0; k <= (width-1)/2; k++)
             {
                 // Calculate width of reflected pixel
                 int newk = width - k - 1;
@@ -85,7 +85,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             }
         }
     }
-    else if (width % 2 !=0)
+    else if (width % 2 != 0)
     {
     // Loop over each pixel on the left-hand side of the image
         for (int i = 0; i < height; i++)
