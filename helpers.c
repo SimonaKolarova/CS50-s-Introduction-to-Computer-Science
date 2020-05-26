841c77362e10112a574b36a1627996a8b7b48846
SimonaKolarova <SimonaKolarova@users.noreply.github.com>
2020-05-26 15:15:53 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -14,7 +14,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int k = 0; k < width; k++)
         {
             // Calculate average brithness
-            float gray = (float) (image[i][k].rgbtRed + image[i][k].rgbtGreen + image[i][k].rgbtBlue) / 3;
+            float gray = (float)(image[i][k].rgbtRed + image[i][k].rgbtGreen + image[i][k].rgbtBlue) / 3;
 
             // Assign average brightness to all RGB values
             image[i][k].rgbtRed = round(gray);
@@ -34,21 +34,21 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int k = 0; k < width; k++)
         {
             // Calculate sepia red colour
-            float sepiaRed = (float) ((0.393 * image[i][k].rgbtRed) + (0.769 * image[i][k].rgbtGreen) + (0.189 * image[i][k].rgbtBlue));
+            float sepiaRed = (float)((0.393 * image[i][k].rgbtRed) + (0.769 * image[i][k].rgbtGreen) + (0.189 * image[i][k].rgbtBlue));
             if (sepiaRed > 255)
             {
                 sepiaRed = 255;
             }
 
             // Calculate sepia  green colour
-            float sepiaGreen = (float) ((0.349 * image[i][k].rgbtRed) + (0.686 * image[i][k].rgbtGreen) + (0.168 * image[i][k].rgbtBlue));
+            float sepiaGreen = (float)((0.349 * image[i][k].rgbtRed) + (0.686 * image[i][k].rgbtGreen) + (0.168 * image[i][k].rgbtBlue));
             if (sepiaGreen > 255)
             {
                 sepiaGreen = 255;
             }
 
             // Calculate sepia blue colour
-            float sepiaBlue = (float) ((0.272 * image[i][k].rgbtRed) + (0.534 * image[i][k].rgbtGreen) + (0.131 * image[i][k].rgbtBlue));
+            float sepiaBlue = (float)((0.272 * image[i][k].rgbtRed) + (0.534 * image[i][k].rgbtGreen) + (0.131 * image[i][k].rgbtBlue));
             if (sepiaBlue > 255)
             {
                 sepiaBlue = 255;
@@ -69,10 +69,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     // Case 1: images of width of even number of pixels
     if (width % 2 == 0)
     {
-    // Loop over each pixel on the left-hand side of the image
+        // Loop over each pixel on the left-hand side of the image
         for (int i = 0; i < height; i++)
         {
-            for (int k = 0; k < width/2; k++)
+            for (int k = 0; k < width / 2; k++)
             {
                 // Calculate width of reflected pixel
                 int newk = width - k - 1;
@@ -89,10 +89,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     // Case 2: images of width of odd number of pixels
     else if (width % 2 != 0)
     {
-    // Loop over each pixel on the left-hand side of the image, including the middle pixel
+        // Loop over each pixel on the left-hand side of the image, including the middle pixel
         for (int i = 0; i < height; i++)
         {
-            for (int k = 0; k <= width/2; k++)
+            for (int k = 0; k <= width / 2; k++)
             {
                 // Calculate width of reflected pixel
                 int newk = width - k - 1;
@@ -141,7 +141,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 Blue += (float) imagetemp[i - 1][k].rgbtBlue;
             }
             
-            if ( k != 0)
+            if (k != 0)
             {
                 Red += (float) imagetemp[i][k - 1].rgbtRed;
                 Green += (float) imagetemp[i][k - 1].rgbtGreen;
@@ -183,6 +183,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 Blue += (float) imagetemp[i - 1][k + 1].rgbtBlue;
 
             }
+            
             if (i != (height - 1) && k != 0)
             {
                 Red += (float) imagetemp[i + 1][k - 1].rgbtRed;
@@ -193,7 +194,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             // Average target pixel value depending on number of added pixels
 
             // Case 1: corner pixels
-            if ((i == 0 || i == (height - 1)) && (k == 0 || k== (width - 1)))
+            if ((i == 0 || i == (height - 1)) && (k == 0 || k == (width - 1)))
             {
                 Red /= 4;
                 Green /= 4;
@@ -208,6 +209,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 Green /= 6;
                 Blue /= 6;
             }
+            
             if ((i > 0 && i < (height - 1)) && (k == 0 || k == (width - 1)))
             {
                 Red /= 6;
@@ -222,12 +224,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 Green /= 9;
                 Blue /= 9;
             }
+            
             // Round all RGB values to integers
-                image[i][k].rgbtRed = round(Red);
-                image[i][k].rgbtGreen = round(Green);
-                image[i][k].rgbtBlue = round(Blue);
+            image[i][k].rgbtRed = round(Red);
+            image[i][k].rgbtGreen = round(Green);
+            image[i][k].rgbtBlue = round(Blue);
         }
     }
-    free (imagetemp);
+    free(imagetemp);
     return;
 }
