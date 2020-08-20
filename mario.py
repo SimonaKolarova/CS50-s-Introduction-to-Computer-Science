9e16189f9a5ada6e26045a7b21ac1810e203d0f8
SimonaKolarova <SimonaKolarova@users.noreply.github.com>
2020-08-20 10:44:46 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,6 +1,6 @@
 from cs50 import get_int
 
-#Get integer n from user
+# Get integer n from user
 n = get_int("Provide a half-pyramid height between 1 and 8: ")
 
 while n < 1 or n > 8:
@@ -9,23 +9,23 @@ while n < 1 or n > 8:
 # Construction of pyramid
 for i in range(n):
 
-    #Make integer m (resiprocal to n)
+    # Make integer m (resiprocal to n)
     m = n-i-1
 
-    #Print m-related spaces
+    # Print m-related spaces
     for j in range(m):
-        print (" ", end ="")
+        print(" ", end="")
 
-    #Print n-related #
+    # Print n-related #
     for k in range(i+1):
-        print ("#", end ="")
+        print("#", end="")
         
     # Print middle spaces
-    print ("  ", end = "")
+    print("  ", end ="")
     
-    #Print n-related #
+    # Print n-related #
     for o in range(i+1):
-        print ("#", end ="")
+        print("#", end="")
         
-    #Print new line
+    # Print new line
     print("")
